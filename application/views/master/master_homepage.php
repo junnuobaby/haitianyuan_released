3d56518089183a0c4ce04c0b3bd340ea1d5d93e1
tangchaohong <444092487@qq.com>
2015-10-12 17:57:02 +0800

    hahhaah

--- application/views/master/master_homepage.php
+++ application/views/master/master_homepage.php
@@ -92,8 +92,8 @@ $signature = $info['signature'];
         <div class="col-md-8 col-sm-8 bg-white block-radius">
             <div class="sub_nav">
                 <ul class="nav nav-tabs" role="tablist">
-                    <li role="presentation" class="active"><a href="#qa" role="tab" data-toggle="tab">问答</a></li>
-                    <li role="presentation"><a href="<?php base_url('index.php/home/load_home/web/' .'master'. '/' . $master_id.'/'.'2');?>" role="tab" data-toggle="tab">观点</a></li>
+                    <li role="presentation" class="active"><a href="#qa" role="tab">问答</a></li>
+                    <li role="presentation"><a href="<?php base_url('index.php/home/load_home/web/' .'master'. '/' . $master_id.'/'.'2');?>">观点</a></li>
                     <li role="presentation"><a href="#demonstration" role="tab" data-toggle="tab">示范</a></li>
                     <li role="presentation"><a href="#contest" role="tab" data-toggle="tab">华山论剑</a></li>
                     <li role="presentation"><a href="#bbs" role="tab" data-toggle="tab">论坛</a></li>
