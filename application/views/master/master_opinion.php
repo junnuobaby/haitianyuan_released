c65ab8c465e2d5e55847480502627e68720b734e
tangchaohong <444092487@qq.com>
2015-10-12 18:10:07 +0800

    hahhaah

--- application/views/master/master_opinion.php
+++ application/views/master/master_opinion.php
@@ -92,7 +92,7 @@ $signature = $info['signature'];
         <div class="col-md-8 col-sm-8 bg-white block-radius">
             <div class="sub_nav">
                 <ul class="nav nav-tabs" role="tablist">
-                    <li role="presentation" ><a href="<?php base_url('index.php/home/load_home/web/' .'master'. '/' . $master_id.'/'.'1');?>" >问答</a></li>
+                    <li role="presentation" ><a href="<?php echo base_url('index.php/home/load_home/web/' .'master'. '/' . $master_id.'/'.'1');?>" >问答</a></li>
                     <li role="presentation" class="active"><a href="#">观点</a></li>
                     <li role="presentation"><a href="#">示范</a></li>
                     <li role="presentation"><a href="#" >华山论剑</a></li>
