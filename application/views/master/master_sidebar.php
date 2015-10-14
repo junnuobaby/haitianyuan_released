5cc487c6d161548497b756fb661049ae5a588673
tangchaohong <444092487@qq.com>
2015-10-14 15:28:48 +0800

    得到

--- application/views/master/master_sidebar.php
+++ application/views/master/master_sidebar.php
@@ -72,7 +72,7 @@ $basic_info = array(
 
                 <div class="list-group">
                     <a href="#" class="list-group-item">我的钱包</a>
-                    <a href="<?php echo base_url("index.php/modify_info/index"); ?>" class="list-group-item">修改个人资料</a>
+                    <a href="<?php echo base_url('index.php/modify_info/index'); ?>" class="list-group-item">修改个人资料</a>
                 </div>
             </li>
         </ul>
