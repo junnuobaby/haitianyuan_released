524fe004a5dcf4c26e0f86283e769e78dc4a679c
tangchaohong <444092487@qq.com>
2015-10-15 16:42:53 +0800

    update

--- application/views/master/published_opinion.php
+++ application/views/master/published_opinion.php
@@ -10,7 +10,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <?php
 $view_list = $op;
 ?>
-
 <div class="wrapper">
     <?php $this->load->view('./templates/navbar'); ?>
     <div class="container container_to_top">
@@ -22,49 +21,49 @@ $view_list = $op;
                 <div class="panel panel-success">
                     <div class="panel-heading"><h3><span class="glyphicon glyphicon-list"></span> 已发表观点</h3>
                     </div>
-                    <div class="panel-body">
-                        <div class="opinion_category">
-                            <label for="kword">提示: 点击带红色下划线的字可更改权限</label>
-                        </div>
-                    </div>
-                    <!-- 已发表的观点列表 -->
-                    <div class="opinion_list">
-                        <?php if(count($view_list) < 1):?>
+                    <?php if (count($view_list) < 1): ?>
                         <h3>您还没有发布任何观点！</h3>
-                        <?php else:?>
-                        <?php foreach ($view_list as $view_item): ?>
-                            <div class="master_view">
-                                <article>
-                                    <header>
-                                        <h4 class="inline_div"><a
-                                                href="#"
-                                                target="_blank"> <?php echo $view_item['op_title']; ?></a></h4>
-                                        <span class="key_word"><?php echo $view_item['op_kwords']; ?></span>
-                                    </header>
-                                    <section>
-                                        <!--控制最多显示内容不超过100个字-->
-                                        <p><?php if (strlen($view_item['op_content']) >= 150) {
-                                                echo mb_substr($view_item['op_content'], 0, 150, 'utf-8') . '...';
-                                            } else {
-                                                echo $view_item['op_content'];
-                                            }
-                                            ?>
-                                        </p>
-                                    </section>
-                                    <section class="view_footer">
+                    <?php else: ?>
+                        <div class="panel-body">
+                            <div class="opinion_category">
+                                <label for="kword">提示: 点击带红色下划线的字可更改权限</label>
+                            </div>
+                        </div>
+                        <!-- 已发表的观点列表 -->
+                        <div class="opinion_list">
+                            <?php foreach ($view_list as $view_item): ?>
+                                <div class="master_view">
+                                    <article>
+                                        <header>
+                                            <h4 class="inline_div"><a
+                                                    href="#"
+                                                    target="_blank"> <?php echo $view_item['op_title']; ?></a></h4>
+                                            <span class="key_word"><?php echo $view_item['op_kwords']; ?></span>
+                                        </header>
+                                        <section>
+                                            <!--控制最多显示内容不超过100个字-->
+                                            <p><?php if (strlen($view_item['op_content']) >= 150) {
+                                                    echo mb_substr($view_item['op_content'], 0, 150, 'utf-8') . '...';
+                                                } else {
+                                                    echo $view_item['op_content'];
+                                                }
+                                                ?>
+                                            </p>
+                                        </section>
+                                        <section class="view_footer">
                                         <span>
                                             <a href="#" class="x-editable-access edit_view"
                                                data-value="<?php echo $view_item['op_auth']; ?>" data-type="select"
                                                data-pk="<?php echo $view_item['op_id']; ?>"
                                                data-title="更改权限"></a>
                                         </span>
-                                    </section>
-                                </article>
-                            </div>
-                            <hr/>
-                        <?php endforeach; ?>
-                        <?php endif;?>
-                    </div>
+                                        </section>
+                                    </article>
+                                </div>
+                                <hr/>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -81,17 +80,17 @@ $view_list = $op;
         //设置x-editable的权限
         $('a.x-editable-access').editable({
             name: 'change_auth',
-            url:'<?php echo base_url("index.php/opinion/auth/"); ?>',
+            url: '<?php echo base_url("index.php/opinion/auth/"); ?>',
             source: [
                 {value: 0, text: '仅VIP可见'},
                 {value: 1, text: '免费'},
             ],
-            success: function success(response, newValue){
-                if(response.status == 'error') {
+            success: function success(response, newValue) {
+                if (response.status == 'error') {
                     return response.msg;
                 }
             },
-            error:function error (response, newValue) {
+            error: function error(response, newValue) {
                 return '服务器故障。';
             }
         });
