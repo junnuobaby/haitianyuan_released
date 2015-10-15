95defcbc68c5df9f0cfc24de35682862a1cd871c
tangchaohong <444092487@qq.com>
2015-10-15 17:44:13 +0800

    update

--- application/views/user/user_sidebar.php
+++ application/views/user/user_sidebar.php
@@ -37,7 +37,7 @@ $basic_info = array(
                 <h4><i class="glyphicon glyphicon-edit"></i> 问答管理</h4>
 
                 <div class="list-group">
-                    <a href="<?php echo base_url('modify_info/get_questions/web/undo');?>" class="list-group-item">我的提问</a>
+                    <a href="<?php echo base_url('index.php/modify_info/get_questions/web/undo');?>" class="list-group-item">我的提问</a>
                 </div>
             </li>
             <li class="list-group-item ">
