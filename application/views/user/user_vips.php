0a1b803e90d3a61da1f2d17e277a5709cf83dbd8
tangchaohong <444092487@qq.com>
2015-10-16 10:28:34 +0800

    update

--- application/views/user/user_vips.php
+++ application/views/user/user_vips.php
@@ -53,7 +53,7 @@ $vips = $master_info;
                                         <p>个性签名：<?php echo $vip['signature']; ?></p>
                                     </div>
                                     <div class="col-md-4">
-                                        <a class="btn btn-success self-btn-danger qu_btn" data-id="<?php echo $vip['master_id'];?>" data-name="<?php echo $vip['master_name'];?>"><span
+                                        <a class="btn btn-success self-btn-danger qu_btn_id" data-id="<?php echo $vip['master_id'];?>" data-name="<?php echo $vip['master_name'];?>"><span
                                                 class="glyphicon glyphicon-question-sign"></span> 提问
                                         </a>
                                     </div>
@@ -111,7 +111,7 @@ $vips = $master_info;
     });
     $(document).ready(function () {
         //点击提问按钮触发的事件
-        $('.qu_btn').click(function () {
+        $('.qu_btn_id').click(function () {
             var state;
             var master_id = $(this).data('id');
             var username = $(this).data('name');
