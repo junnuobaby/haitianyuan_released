b2fc83979b0522c0204e6f1fbb2d3cdadb4b03d2
tangchaohong <444092487@qq.com>
2015-10-30 17:55:34 +0800

    update

--- application/views/master/master_vips.php
+++ application/views/master/master_vips.php
@@ -93,38 +93,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         $('.his_qa_btn').click(function () {
             counter = 1;
             var id = $(this).data('id');
+            $('#records_modal').data('id', id);
             alert(id);
             $.ajax({
                 url: '<?php echo base_url("index.php/modify_info/get_user_qa"); ?>' +'/' + 'web'+'/' + $(this).data('id'),
                 data: {page: 1},
                 method: 'get',
-                success: function () {
-                    var content = "";
-                    var qa_list = data['qa_info']['data_page']; //获取历史问题记录内容
-                    var count = data['qa_info']['count']; //获取历史回答记录的数量
-                    var page_count = data['qa_info']['page_count']; //获取页面的数量
-                    for(var i= 0; i< qa_list.length; i++){
-                        content += '<div class="q_a qu_margin">' +
-                            '<article>' +
-                            '<h4 class="q_a_question inline_block">' +
-                            '<span class="q_a_span">问</span>' +
-                            '<a href="#">' + qa_list[i]['qu_content'] + '</a></h4>' +
-                            '<span class="qu_time">【' + qa_list[i]['qu_timestamp'] + '】</span>' +
-                            '<p class="q_a_answer"><span class="theme-color">答:</span>&nbsp;&nbsp;' + qa_list[i]['ans_content'] +
-                            '</p>' +
-                            '<div class="q_a_footer">' +
-                            '<span>回答时间：' + qa_list[i]['ans_timestamp'] + '</span>' +
-                            '</div>' +
-                            '</article>' +
-                            '</div>' +
-                            '<hr class="q_a_hr"/>';
-                    }
-                    if(page_count > counter){
-                        content += '<a class="btn btn-success btn-show-more show_block center-block">点击查看更多</a>';
-                        $('a.btn-show-more').data('id', id);
-                    }
-                    $('#his_qa').html(content);
-                },
+                success: show_his_qa,
                 error:function(){
                     alert('亲，对不起，我们的服务器宝宝生病了');
                 },
@@ -154,7 +129,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             }
             if(page_count > counter){
                 content += '<a class="btn btn-success btn-show-more show_block center-block">点击查看更多</a>'
-                $('a.btn-show-more').data('id', id);
             }
             $('#his_qa').html(content);
         }
@@ -164,7 +138,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             counter += 1;
             $('a.btn-show-more').hide();
             $.ajax({
-                url: '<?php echo base_url("index.php/modify_info/get_user_qa"); ?>' +'/' + 'web'+'/' + $(this).data('id'),
+                url: '<?php echo base_url("index.php/modify_info/get_user_qa"); ?>' +'/' + 'web'+'/' + $('#records_modal').data('id'),
                 data: {page: counter},
                 method: 'get',
                 success: show_more_qa,
@@ -194,7 +168,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             }
             if(page_count > counter){
                 content += '<a class="btn btn-success btn-show-more show_block center-block">点击查看更多</a>'
-                $('a.btn-show-more').data('id', id);
             }
             $('#his_qa').append(content);
         }
