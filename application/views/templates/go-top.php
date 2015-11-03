ee8a64f3cd9d2b934822485ba7dddec0cd7af013
tangchaohong <444092487@qq.com>
2015-11-03 15:09:53 +0800

    update

--- application/views/templates/go-top.php
+++ application/views/templates/go-top.php
@@ -1,5 +1,22 @@
-<!-- 悬停页面右下脚的按钮-->
-<div class="fixed-bar">
-    <a class="go-top" href="#" title="返回顶部"><i class="glyphicon glyphicon-chevron-up"></i></a>
-    <a class="ht_feedback" href="#" title="反馈意见">反馈</a>
-</div>
+<ul class="fixed-bar">
+    <li class="feedback">
+        <a class="fixed-bar-icon"><i class="glyphicon glyphicon-question-sign hz-icon"></i></a>
+        <a class="fixed-bar-text">问题反馈</a>
+    </li>
+    <li class="go-top">
+        <a class="fixed-bar-icon"><i class="glyphicon glyphicon-chevron-up hz-icon"></i></a>
+        <a href="#" class="fixed-bar-text">回到顶部</a>
+    </li>
+</ul>
+
+<script>
+    $(document).ready(function(){
+        $('ul.fixed-bar li').hover(function(){
+            $(this).children(':eq(0)').hide();
+            $(this).css('background-color', '#f4645f').children(':eq(1)').show().css('display', 'block');
+        }, function () {
+            $(this).children(':eq(1)').hide();
+            $(this).css('background-color', '#fff').children(':eq(0)').show().css('display', 'block');
+        });
+    });
+</script>
