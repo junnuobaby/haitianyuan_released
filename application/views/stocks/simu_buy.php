5a8ad185f22385a0061d7d6e5d2d2a2b226f5a45
tangchaohong <444092487@qq.com>
2015-11-05 20:36:52 +0800

    update

--- application/views/stocks/simu_buy.php
+++ application/views/stocks/simu_buy.php
@@ -48,8 +48,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                 <label for="buy_price" class="col-sm-4 control-label">买入价格:</label>
 
                                                 <div class="col-sm-8">
-                                                    <input type="text" class="form-control" id="buy_price"
-                                                           name="buy_price">
+                                                    <div class="input-group">
+                                                        <input type="text" class="form-control" id="buy_price"
+                                                               name="buy_price">
+                                                        <div class="input-group-addon">¥</div>
+                                                    </div>
+
                                                 </div>
                                             </div>
                                             <div class="form-group">
