cdc09651aa9eec06cf778f108a37bf38fedd0de0
tangchaohong <444092487@qq.com>
2015-11-05 21:01:35 +0800

    update

--- application/views/stocks/trading_rules.php
+++ application/views/stocks/trading_rules.php
@@ -82,6 +82,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         $('section').css('max-width', '800px');
         $('section h5').addClass('theme-color');
         $('section ol').css('line-height','2em');
+        $('section ol').css('margin-left','2em');
         $('section p').css('text-indent','2em');
 
     });
