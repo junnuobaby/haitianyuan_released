2937d5aa3e2bef5e53c24b329b00ca27a1ec5e0f
tangchaohong <444092487@qq.com>
2015-11-06 20:22:43 +0800

    update

--- application/views/stocks/simu_buy.php
+++ application/views/stocks/simu_buy.php
@@ -170,6 +170,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </body>
 <script>
     var interval;
+    var count_times = 0;
     $(document).ready(function () {
         $('.main_jumptron').css('margin-bottom', '0px');
     });
@@ -289,6 +290,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
         //在鼠标点击选中和enter键之后调用该函数
         function selected_code_info(code) {
+            count_times += 1;
             $.ajax({
                 url: '<?php echo base_url("index.php/stock/get_bs/web"); ?>' + '/' + code,
                 method: 'get',
@@ -342,12 +344,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     }
                 });
                 //将当前价格设置为默认的买入价格
-                $('#buy_price').val(bond_cur_price);
-                var bond_price = $('#buy_price').val(); //买入价格
-                var available_money = "<?php echo $cash_use;?>";
-                var quantity_avail = Math.round(parseFloat(available_money) / (parseFloat(bond_price) * 100)); //计算当前可买入的最大股数
-                $('div.largest_quantity').removeClass('hidden');
-                $('#largest_quantity').html(quantity_avail);
+                if(count_times == 1){
+                    $('#buy_price').val(bond_cur_price);
+                    var bond_price = $('#buy_price').val(); //买入价格
+                    var available_money = "<?php echo $cash_use;?>";
+                    var quantity_avail = Math.round(parseFloat(available_money) / (parseFloat(bond_price) * 100)); //计算当前可买入的最大股数
+                    $('div.largest_quantity').removeClass('hidden');
+                    $('#largest_quantity').html(quantity_avail);
+                }
             }
         }
 
