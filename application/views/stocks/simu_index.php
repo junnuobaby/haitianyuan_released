9822f39ef68f79c47ca90a83ac5425a42ab53f62
tangchaohong <444092487@qq.com>
2015-11-16 17:55:03 +0800

    update

--- application/views/stocks/simu_index.php
+++ application/views/stocks/simu_index.php
@@ -195,7 +195,7 @@ $user_stocks = $user_info['data_stock']; //获取用户持仓数据
                                     $(tr_id).children('td:eq(8)').css('color','red');
                                 }
                                 $(tr_id).children('td:eq(5)').html(trade_price);  //设置当前价
-                                $(tr_id).children('td:eq(6)').html(stock_info[key]['float_pl']);   //设置浮动盈亏
+                                $(tr_id).children('td:eq(6)').html(parseFloat(stock_info[key]['float_pl']).toFixed(2));   //设置浮动盈亏
                                 $(tr_id).children('td:eq(7)').html(parseFloat(stock_info[key]['float_pl_rate'] ).toFixed(2) + '%');   //设置盈亏比
                                 $(tr_id).children('td:eq(8)').html(id_extent + '%');  //设置涨跌幅
                             }
