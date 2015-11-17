e02b5f0ea1b0704ea1bd62ee6c40cb0793031216
tangchaohong <444092487@qq.com>
2015-11-17 18:04:49 +0800

    update

--- application/views/stocks/graph.php
+++ application/views/stocks/graph.php
@@ -125,7 +125,7 @@
                     },
                     series : [
                         {
-                            name:'访问来源',
+                            name:'资产分布',
                             type:'pie',
                             radius : '55%',
                             center: ['50%', '60%'],
