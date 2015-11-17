1b95ac091c7d31ec9dfdb861be06000bb365e28f
tangchaohong <444092487@qq.com>
2015-11-17 12:22:58 +0800

    update

--- application/views/stocks/simu_buy.php
+++ application/views/stocks/simu_buy.php
@@ -373,10 +373,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         $('#buy_price').blur(function () {
             var bond_price = $.trim($(this).val());
             var available_money = "<?php echo $cash_use;?>";
-            if(isNaN(bond_price)){
+            if(isNaN(bond_price) || parseFloat(bond_price) < 0){
                 $('span.buy_price_alert').removeClass('hidden');
             }
-            if(bond_price.length > 0 && !isNaN(bond_price)){
+            if(bond_price.length > 0 && !isNaN(bond_price) && parseFloat(bond_price) > 0){
                 bond_price = decimal(bond_price);
                 var quantity_avail = parseInt(parseFloat(available_money) / (parseFloat(bond_price) * 100)); //计算当前可买入的最大股数
                 $('div.largest_quantity').removeClass('hidden');
