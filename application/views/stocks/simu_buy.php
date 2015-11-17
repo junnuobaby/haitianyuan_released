dc6fd8aa4daacddf47333b8ee412c6f78ea85efa
tangchaohong <444092487@qq.com>
2015-11-17 21:13:02 +0800

    update

--- application/views/stocks/simu_buy.php
+++ application/views/stocks/simu_buy.php
@@ -253,6 +253,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         //响应上下键事件
         function navigate_list(event) {
             var cur = $('div.hint_list tr.hint_active');
+            alert(cur.length);
             var bond_code = $('#bond_code');
             switch (event.which) {
                 case 38:   //上键
