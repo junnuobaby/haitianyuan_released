68fbcbfc15b26f2a7f4cc4a05732e73b960e897d
tangchaohong <444092487@qq.com>
2015-11-17 11:40:39 +0800

    update

--- application/views/stocks/simu_buy.php
+++ application/views/stocks/simu_buy.php
@@ -329,6 +329,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 var sell_1ist = [response.SellPrice1, response.SellPrice2, response.SellPrice3, response.SellPrice4, response.SellPrice5]; //卖五
                 var buy_1ist = [response.BuyPrice1, response.BuyPrice2, response.BuyPrice3, response.BuyPrice4, response.BuyPrice5];  //买五
 
+                for(var j= 0; j < sell_1ist.length; j++){
+                    sell_1ist[j] = decimal(sell_1ist[j]);
+                }
+                for(var k= 0; k < sell_1ist.length; k++){
+                    buy_1ist[k] = decimal(buy_1ist[k]);
+                }
 
 
                 $('#bond_name').html(bond_name);
