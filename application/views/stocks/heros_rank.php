d3a544aa75fa64044db32a3ccb85ffea1704fb27
tangchaohong <444092487@qq.com>
2015-11-18 16:17:09 +0800

    update

--- application/views/stocks/heros_rank.php
+++ application/views/stocks/heros_rank.php
@@ -36,7 +36,7 @@ $ranking = 0;
                 <div class="stocks_min_h  block-radius">
                     <!--宗师级别-->
                     <div class="rank_table">
-                        <div class="col-md-8 col-md-offset-2">
+                        <div class="col-md-8 col-md-offset-4">
                             <div class="plaque">
                                 <img width="70px" height="70px" class="img-responsive inline_block"
                                      src="<?php echo base_url('/assets/images/zongshi.png'); ?>" alt="Logo加载中...">
