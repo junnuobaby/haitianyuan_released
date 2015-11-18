cc301b16e390c932441b72c06d95d2f45f755a58
tangchaohong <444092487@qq.com>
2015-11-18 11:54:41 +0800

    update

--- application/views/stocks/heros_rank.php
+++ application/views/stocks/heros_rank.php
@@ -21,8 +21,8 @@ $ranking = 0;
                     <div class="rank_table">
                         <table class="table table-bordered table-condensed table-hover">
                             <thead>
-                            <tr class="warning">
-                                <td>排名</td>
+                            <tr>
+                                <th>排名</th>
                                 <th>用户名</th>
                                 <th>总资产</th>
                                 <th>仓位</th>
