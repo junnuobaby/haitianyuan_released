66963ff2687fa26c91312bbe50e4106f0773d8bf
tangchaohong <444092487@qq.com>
2015-11-18 11:08:25 +0800

    update

--- application/views/stocks/heros_rank_sidebar.php
+++ application/views/stocks/heros_rank_sidebar.php
@@ -1,14 +1,11 @@
 <div>
-    <ul class="list-group">
-        <li class="list-group-item">
-            <h5>排行榜</h5>
-            <div class="list-group">
-                <a href="#" class="list-group-item">日收益率排名</a>
-                <a href="#" class="list-group-item">周收益率排名</a>
-                <a href="#" class="list-group-item">月收益率排名</a>
-                <a href="#" class="list-group-item">总收益率排名</a>
-            </div>
-        </li>
-    </ul>
-
+    <div class="list-group">
+        <a href="#" class="list-group-item active">
+           排行榜
+        </a>
+        <a href="#" class="list-group-item">日收益率排名</a>
+        <a href="#" class="list-group-item">周收益率排名</a>
+        <a href="#" class="list-group-item">月收益率排名</a>
+        <a href="#" class="list-group-item">总收益率排名</a>
+    </div>
 </div>
