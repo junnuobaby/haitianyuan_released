e5ccf17d5df0460d39d7b1e31825a48300dcf15f
tangchaohong <444092487@qq.com>
2015-11-18 11:26:29 +0800

    update

--- application/views/stocks/heros_rank.php
+++ application/views/stocks/heros_rank.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="zh-cn">
 <?php $this->load->view('./templates/head'); ?>
 <?php
-
+$hero_lists = $heros_list;  //获取排行榜前100数据
 ?>
 <body class="bg-gray">
 <div class="wrapper">
@@ -17,7 +17,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </div>
             <div class="col-md-10 col-sm-10">
                 <div class="bg-white stocks_min_h  block-radius">
-
+                    <table class="table table-bordered table-condensed table-hover">
+                        <thead>
+                        <tr>
+                            <th>用户名</th>
+                            <th>总资产</th>
+                            <th>仓位</th>
+                            <th>总收益率</th>
+                            <th>日收益率</th>
+                            <th>周收益率</th>
+                            <th>月收益率</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($hero_lists as $item):?>
+                        <tr>
+                            <td><?= $item['user_name'];?></td>
+                            <td><?= $item['fund'];?></td>
+                            <td><?= $item['position'];?></td>
+                            <td><?= $item['profit_rate'];?></td>
+                            <td><?= $item['day_rate'];?></td>
+                            <td><?= $item['week_rate'];?></td>
+                            <td><?= $item['month_rate'];?></td>
+                        </tr>
+                        <?php endforeach;?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
--- application/views/stocks/heros_rank_sidebar.php
+++ application/views/stocks/heros_rank_sidebar.php
@@ -3,9 +3,9 @@
         <a href="#" class="list-group-item active">
            排行榜
         </a>
-        <a href="#" class="list-group-item">日收益率排名</a>
-        <a href="#" class="list-group-item">周收益率排名</a>
-        <a href="#" class="list-group-item">月收益率排名</a>
-        <a href="#" class="list-group-item">总收益率排名</a>
+        <a href="<?php echo base_url('/index.php/stock/index/web/2/1');?>" class="list-group-item">总收益率排名</a>
+        <a href="<?php echo base_url('/index.php/stock/index/web/2/2');?>" class="list-group-item">日收益率排名</a>
+        <a href="<?php echo base_url('/index.php/stock/index/web/2/3');?>" class="list-group-item">周收益率排名</a>
+        <a href="<?php echo base_url('/index.php/stock/index/web/2/4');?>" class="list-group-item">月收益率排名</a>
     </div>
 </div>
