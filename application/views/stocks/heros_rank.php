68e4e614826b29e8008c90f1530837aede514679
tangchaohong <444092487@qq.com>
2015-11-18 17:45:13 +0800

    update

--- application/views/stocks/heros_rank.php
+++ application/views/stocks/heros_rank.php
@@ -30,8 +30,14 @@ $ranking = 0;
     <div class="container">
         <div class="row">
             <div class="col-md-2">
-                <?php $this->load->view('./stocks/heros_rank_sidebar'); ?>
-            </div>
+                <div id="ranking_sidebar" class="flow-sidebar" data-spy="affix" data-offset-top='100'>
+                    <div class="list-group">
+                        <a href="<?php echo base_url('/index.php/stock/index/web/2/1');?>" class="list-group-item <?php if($nav_mode == "simu_heros_1") echo "active"; ?>"><span class="glyphicon glyphicon-star"></span> 总收益率排名</a>
+                        <a href="<?php echo base_url('/index.php/stock/index/web/2/2');?>" class="list-group-item <?php if($nav_mode == "simu_heros_2") echo "active"; ?>"><span class="glyphicon glyphicon-asterisk"></span> 日收益率排名</a>
+                        <a href="<?php echo base_url('/index.php/stock/index/web/2/3');?>" class="list-group-item <?php if($nav_mode == "simu_heros_3") echo "active"; ?>"><span class="glyphicon glyphicon-th-large"></span> 周收益率排名</a>
+                        <a href="<?php echo base_url('/index.php/stock/index/web/2/4');?>" class="list-group-item <?php if($nav_mode == "simu_heros_4") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
+                    </div>
+                </div>            </div>
             <div class="col-md-10 col-sm-10 bg-white ">
                 <div class="stocks_min_h  block-radius">
                     <!--禅师级别-->
