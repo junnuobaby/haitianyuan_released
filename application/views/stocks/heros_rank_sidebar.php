86c3104896f0869690d59f6e61248e5abcbd99a8
tangchaohong <444092487@qq.com>
2015-11-18 15:16:32 +0800

    update

--- application/views/stocks/heros_rank_sidebar.php
+++ application/views/stocks/heros_rank_sidebar.php
@@ -1,11 +1,11 @@
 <div>
     <div class="list-group">
-        <a href="#" class="list-group-item active">
+        <a href="#" class="list-group-item disabled">
            排行榜
         </a>
-        <a href="<?php echo base_url('/index.php/stock/index/web/2/1');?>" class="list-group-item">总收益率排名</a>
-        <a href="<?php echo base_url('/index.php/stock/index/web/2/2');?>" class="list-group-item">日收益率排名</a>
-        <a href="<?php echo base_url('/index.php/stock/index/web/2/3');?>" class="list-group-item">周收益率排名</a>
-        <a href="<?php echo base_url('/index.php/stock/index/web/2/4');?>" class="list-group-item">月收益率排名</a>
+        <a href="<?php echo base_url('/index.php/stock/index/web/2/1');?>" class="list-group-item <?php if($nav_mode == "simu_heros_1") echo "active"; ?>">总收益率排名</a>
+        <a href="<?php echo base_url('/index.php/stock/index/web/2/2');?>" class="list-group-item <?php if($nav_mode == "simu_heros_2") echo "active"; ?>">日收益率排名</a>
+        <a href="<?php echo base_url('/index.php/stock/index/web/2/3');?>" class="list-group-item <?php if($nav_mode == "simu_heros_3") echo "active"; ?>">周收益率排名</a>
+        <a href="<?php echo base_url('/index.php/stock/index/web/2/4');?>" class="list-group-item <?php if($nav_mode == "simu_heros_4") echo "active"; ?>">月收益率排名</a>
     </div>
 </div>
