dc11b54068b3878b57d8ea78a590ef7e632f454a
tangchaohong <444092487@qq.com>
2015-11-18 10:35:56 +0800

    update

---
+++ application/views/stocks/heros_list.php
@@ -0,0 +1,44 @@
+<!--模拟炒股，已成交的订单-->
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="zh-cn">
+<?php $this->load->view('./templates/head'); ?>
+<?php
+
+?>
+<body class="bg-gray">
+<div class="wrapper">
+    <?php $this->load->view('./stocks/bonds_navbar'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <?php $this->load->view('./stocks/heros_list_sidebar'); ?>
+            </div>
+            <div class="col-md-9 col-sm-9">
+                <div class="bg-white stocks_min_h  block-radius">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--悬停go-top按钮-->
+    <?php $this->load->view('./templates/go-top'); ?>
+</div>
+<?php $this->load->view('./templates/footer'); ?>
+</body>
+<script>
+    $(document).ready(function () {
+        $('.main_jumptron').css('margin-bottom', '0px');
+        $('.formatted').each(function(){
+            var value = format_num($(this).html());
+            $(this).html(value);
+        });
+        $('.decimal').each(function () {
+            var value = decimal($(this).html());
+            $(this).html(value);
+        });
+    });
+</script>
+</html>
---
+++ application/views/stocks/heros_list_sidebar.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="list-group">
+        <a href="#" class="list-group-item active">
+           排行榜
+        </a>
+        <a href="#" class="list-group-item">日收益率排名</a>
+        <a href="#" class="list-group-item">周收益率排名</a>
+        <a href="#" class="list-group-item">月收益率排名</a>
+        <a href="#" class="list-group-item">总收益率排名</a>
+    </div>
+</div>
