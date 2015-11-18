88f5cb84c77acca5925c167df17d15533a268685
tangchaohong <444092487@qq.com>
2015-11-18 10:56:11 +0800

    update

--- application/views/stocks/bonds_navbar.php
+++ application/views/stocks/bonds_navbar.php
@@ -7,7 +7,7 @@
                     <ul class="stocks_menu">
                         <li><a class="white-color" href="<?php echo base_url('/index.php/index');?>">首页</a></li>
                         <li class="<?php if($nav_mode == "simu_index") echo "nav_current"; ?>"><a href="<?php echo base_url('/index.php/stock/index/web/1/1');?>">模拟投资</a></li>
-                        <li class="<?php if($nav_mode == "simu_heros") echo "nav_current"; ?>"><a href="<?php echo base_url('/index.php/stock/get_heros/web/1');?>">英雄榜</a></li>
+                        <li class="<?php if($nav_mode == "simu_heros") echo "nav_current"; ?>"><a href="<?php echo base_url('/index.php/stock/get_heros/web/1/1');?>">英雄榜</a></li>
                         <li class="<?php if($nav_mode == "trading_rules") echo "nav_current"; ?>"><a href="<?php echo base_url('/index.php/stock/index/web/3');?>">交易规则</a></li>
                         <li class="<?php if($nav_mode == "trading_guides") echo "nav_current"; ?>"><a href="<?php echo base_url('/index.php/stock/index/web/4');?>">操作指南</a></li>
                         <li><a href="#">帮助中心</a></li>
