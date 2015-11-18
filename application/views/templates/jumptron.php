c63cf9091f5ada327f7df9e633adf68a201d34c2
tangchaohong <444092487@qq.com>
2015-11-18 18:10:02 +0800

    update

--- application/views/templates/jumptron.php
+++ application/views/templates/jumptron.php
@@ -5,12 +5,12 @@
         <div class="row">
             <div class="col-md-3 col-sm-3">
                 <div class="logo_pic">
-                    <img class="img-responsive" src="<?php echo base_url('/assets/images/zhuge0.png'); ?>" alt="Logo加载中...">
+<!--                    <img class="img-responsive" src="--><?php //echo base_url('/assets/images/zhuge0.png'); ?><!--" alt="Logo加载中...">-->
                 </div>
             </div>
             <div class="col-md-9 col-sm-9">
                 <div class="logo">
-                    <img class="img-responsive" src="<?php echo base_url('/assets/images/slogan1.png'); ?>" alt="Logo加载中...">
+<!--                    <img class="img-responsive" src="--><?php //echo base_url('/assets/images/slogan1.png'); ?><!--" alt="Logo加载中...">-->
                 </div>
             </div>
         </div>
@@ -20,6 +20,8 @@
 <style type="text/css">
     .main_jumptron
     {
-        background-image: url("<?php echo base_url('assets/images/jumptron_background.jpg'); ?>");
+/*        background-image: url("*/<?php //echo base_url('assets/images/jumptron_background.jpg'); ?>/*");*/
+        background-image: url("<?php echo base_url('assets/images/b_1_1.png'); ?>");
+        height: 480px;
     }
 </style>
