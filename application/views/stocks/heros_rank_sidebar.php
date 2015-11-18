11f4221e8d5767435b3841814d9686a1ddc7adda
tangchaohong <444092487@qq.com>
2015-11-18 15:24:14 +0800

    update

--- application/views/stocks/heros_rank_sidebar.php
+++ application/views/stocks/heros_rank_sidebar.php
@@ -1,8 +1,5 @@
 <div>
     <div class="list-group">
-        <span class="list-group-item" style="background-color: rgb(57, 109, 129);">
-           排行榜
-        </span>
         <a href="<?php echo base_url('/index.php/stock/index/web/2/1');?>" class="list-group-item <?php if($nav_mode == "simu_heros_1") echo "active_list_item"; ?>">总收益率排名</a>
         <a href="<?php echo base_url('/index.php/stock/index/web/2/2');?>" class="list-group-item <?php if($nav_mode == "simu_heros_2") echo "active"; ?>">日收益率排名</a>
         <a href="<?php echo base_url('/index.php/stock/index/web/2/3');?>" class="list-group-item <?php if($nav_mode == "simu_heros_3") echo "active"; ?>">周收益率排名</a>
