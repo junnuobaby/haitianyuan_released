b9047eb421a022c4379939f39d5856fecc2fc628
tangchaohong <444092487@qq.com>
2015-11-18 15:00:16 +0800

    update

--- application/views/stocks/simu_cancel.php
+++ application/views/stocks/simu_cancel.php
@@ -41,33 +41,36 @@ $pages = $pre_list['pagination']; //获取分页
                                         <tbody>
                                         <?php foreach ($records as $stock_item): ?>
                                             <tr class="done_record">
-                                                <!--                                                时间-->
+                                                <!--时间-->
                                                 <td><?php echo $stock_item['timestamp']; ?></td>
-                                                <!--                                                证券代码-->
-                                                <td><?php echo $stock_item['SecurityID']; ?></td>
-                                                <!--                                                证券名称-->
+                                                <!--证券代码-->
+                                                <td data-toggle="modal" data-target="#graphModal"
+                                                    onclick="fillimage('<?= $stock_item['SecurityID']; ?>', '<?= $stock_item['Symbol']; ?>')">
+                                                    <a href="#"
+                                                       class="hty_a"><?php echo $stock_item['SecurityID']; ?></a></td>
+                                                <!--证券名称-->
                                                 <td><?php echo $stock_item['Symbol']; ?></td>
-                                                <!--                                                委托方向-->
+                                                <!--委托方向-->
                                                 <td><?php if ($stock_item['trade_type'] == '0') {
                                                         echo '买入';
                                                     } else {
                                                         echo "卖出";
                                                     } ?></td>
-                                                <!--                                                委托数量-->
+                                                <!--委托数量-->
                                                 <td class="formatted"><?php echo $stock_item['Volume']; ?></td>
-                                                <!--                                                委托价格-->
+                                                <!--委托价格-->
                                                 <td class="formatted"><?php echo $stock_item['Price']; ?></td>
-                                                <!--                                                委托金额-->
+                                                <!--委托金额-->
                                                 <td class="formatted"><?php echo $stock_item['Price'] * $stock_item['Volume']; ?></td>
-                                                <!--                                                预收交易费用（不超过1万统一收5元，超过10000按万三标准收取）-->
+                                                <!--预收交易费用（不超过1万统一收5元，超过10000按万三标准收取）-->
                                                 <td class="formatted"><?php
-                                                    if($stock_item['trade_type'] == '0'){
+                                                    if ($stock_item['trade_type'] == '0') {
                                                         $trade_money = floatval($stock_item['Price'] * $stock_item['Volume']) * 0.0003;
-                                                    }else{
+                                                    } else {
                                                         $trade_money = floatval($stock_item['Price'] * $stock_item['Volume']) * 0.0013;
                                                     }
                                                     if ($trade_money > 5) {
-                                                        echo  number_format($trade_money, 2);
+                                                        echo number_format($trade_money, 2);
                                                     } else {
                                                         echo '5.00';
                                                     }
@@ -92,11 +95,24 @@ $pages = $pre_list['pagination']; //获取分页
     <?php $this->load->view('./templates/go-top'); ?>
 </div>
 <?php $this->load->view('./templates/footer'); ?>
+<div class="modal fade" id="graphModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title" id="graph_modal_title"></h3>
+            </div>
+            <div class="modal-body" id="graph_modal_body">
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 <script>
     $(document).ready(function () {
         $('.main_jumptron').css('margin-bottom', '0');
-        $('.formatted').each(function(){
+        $('.formatted').each(function () {
             var value = format_num($(this).html());
             $(this).html(value);
         });
@@ -123,5 +139,27 @@ $pages = $pre_list['pagination']; //获取分页
 
         });
     });
+    //从新浪获取分时图
+    function fillimage(stock_id, stock_name) {
+        var modal_body = document.getElementById("graph_modal_body");
+        var modal_title = document.getElementById("graph_modal_title");
+        var start_code = stock_id.toString().substr(0, 3);
+        console.log(start_code);
+        if (start_code == '300' || start_code == '000') {
+            stock_id = 'sz' + stock_id;
+        } else if (start_code == '600') {
+            stock_id = 'sh' + stock_id;
+        }
+        else {
+            modal_body.innerHTML = '暂无该数据';
+            return;
+        }
+        modal_body.innerHTML = "<h3>分时图</h3>";
+        modal_body.innerHTML += "<img src='http://image.sinajs.cn/newchart/min/n/" + stock_id + ".gif' />";
+        modal_body.innerHTML += "<hr/>";
+        modal_body.innerHTML += "<h3>K线图</h3>";
+        modal_body.innerHTML += "<img src='http://image.sinajs.cn/newchart/daily/n/" + stock_id + ".gif' />";
+        modal_title.innerHTML = "<h2>" + "<strong>" + stock_name + "</strong>" + "(" + stock_id + ")" + "</h2>"
+    }
 </script>
 </html>
