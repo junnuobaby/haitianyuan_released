d0d12434118e4ea16d59be9a073e3a6b6778c7e6
tangchaohong <444092487@qq.com>
2015-11-19 16:41:36 +0800

    update

--- application/views/stocks/simu_index.php
+++ application/views/stocks/simu_index.php
@@ -210,18 +210,18 @@ $base_funds = $user_data['base_cash'];  //获取用户基本资金
                     var base_funds = parseFloat('<?php echo $base_funds;?>');
                     var user_value = asset_all - base_funds;  //总盈亏额
                     var user_rate = decimal((user_value *100) / base_funds);   //总盈亏率
-                    if (parseFloat(response.pl_value) > 0) {
+                    if (parseFloat(user_value) > 0) {
                         pl_value.css('color', 'red');
                     } else {
                         pl_value.css('color', 'green');
                     }
-                    if (parseFloat(response.pl_rate) > 0) {
+                    if (parseFloat(user_rate) > 0) {
                         pl_rate.css('color', 'red');
                     } else {
                         pl_rate.css('color', 'green');
                     }
                     pl_value.html(format_num(user_value)); //获取并设置总盈亏金额
-                    pl_rate.html(user_rate); //获取并设置总盈亏比
+                    pl_rate.html(user_rate + '%'); //获取并设置总盈亏比
                     var stock_info = response.stock_info;
                     for (key in stock_info) {
                         var tr_id = '#' + key;
