bfef7dc8445e3b609a86f5e8a2abd309740d7465
tangchaohong <444092487@qq.com>
2015-11-19 17:57:49 +0800

    update

--- application/views/stocks/simu_index.php
+++ application/views/stocks/simu_index.php
@@ -30,7 +30,7 @@ $base_funds = $user_data['base_cash'];  //获取用户基本资金
                                         <h4 class="blue-color margin_to_top">我的资金</h4>
                                         <div class="table-responsive">
                                             <table class="table  basic_fund_info">
-                                                <tr class="table_right">
+                                                <tr>
                                                     <th>总资产</th>
                                                     <td id="my_asset"></td>
                                                 </tr>
