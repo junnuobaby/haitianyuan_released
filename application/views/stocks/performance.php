f4227f098cc93ce5f5445d9f4e6675b395c67bb5
tangchaohong <444092487@qq.com>
2015-11-19 18:08:31 +0800

    update

--- application/views/stocks/performance.php
+++ application/views/stocks/performance.php
@@ -5,7 +5,7 @@ $perform_avg = $performance['data_avg'];  //平均历史收益率数据
 $stock_value = floatval($basic_info['fund']) - floatval($basic_info['cash_all']);  //计算持有股票的市值
 $profit_rate = number_format(floatval($basic_info['profit_rate']) * 100, 2);
 $profit_rank = floatval($basic_info['profit_rank']);
-$day_rate = number_format(floatval($basic_info['day_rate']) * 100,2);
+$day_rate = number_format(floatval($basic_info['day_rate']) * 100, 2);
 $day_rank = floatval($basic_info['day_rank']);
 $week_rate = number_format(floatval($basic_info['week_rate']) * 100, 2);
 $week_rank = floatval($basic_info['week_rank']);
@@ -39,21 +39,21 @@ $month_rank = floatval($basic_info['month_rank']);
                                 %
                             </td>
                             <td class="render">
-                                <?php echo $week_rate;?>
+                                <?php echo $week_rate; ?>
                                 %
                             </td>
                             <td class="render">
-                                <?php echo $month_rate;?>
+                                <?php echo $month_rate; ?>
                                 %
                             </td>
                             <td class="render">
-                                <?php echo $profit_rate;?>
+                                <?php echo $profit_rate; ?>
                                 %
                             </td>
                         </tr>
                         <tr>
                             <td>排名</td>
-                            <td><?php echo $day_rank;?></td>
+                            <td><?php echo $day_rank; ?></td>
                             <td><?php echo $week_rank; ?></td>
                             <td><?php echo $month_rank; ?></td>
                             <td>
@@ -64,53 +64,7 @@ $month_rank = floatval($basic_info['month_rank']);
                     </table>
                 </div>
                 <hr/>
-                <div class="asset">
-                    <div class="asset_detail">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <div id="pie_canvas"></div>
-                            </div>
-                            <div class="col-md-5">
-                                <form class="form-horizontal" id="assets_form">
-                                    <div class="form-group">
-                                        <label class="control-label col-sm-4 grey-color">总资产：</label>
-                                        <div class="col-sm-8 align_left">
-                                            <div class="form-control-static">
-                                                <span href="#" class="formatted"><?php echo $basic_info['fund'];?></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-sm-4 grey-color">可用现金：</label>
-                                        <div class="col-sm-8 align_left">
-                                            <div class="form-control-static">
-                                                <span href="#" class="formatted"><?php echo $basic_info['cash_use'];?></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-sm-4 grey-color">冻结金额：</label>
-                                        <div class="col-sm-8 align_left">
-                                            <div class="form-control-static">
-                                                <span href="#" class="formatted"><?php echo $basic_info['cash_freeze'];?></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-sm-4 grey-color">股票市值：</label>
-                                        <div class="col-sm-8 align_left">
-                                            <div class="form-control-static">
-                                                <span href="#" class="formatted"><?php echo $stock_value;?></span>
-                                            </div>
-                                        </div>
-                                    </div>
 
-                                </form>
-                            </div>
-                            <div class="col-md-6"></div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
@@ -157,9 +111,9 @@ $month_rank = floatval($basic_info['month_rank']);
         var cash_freeze = decimal(parseFloat("<?php echo $basic_info['cash_freeze'];?>"));
         var stock_value = decimal(parseFloat("<?php echo $stock_value;?>"));
         var parts_value = [
-            {value:cash_use, name:'可用现金'},
-            {value:cash_freeze, name:'冻结金额'},
-            {value:stock_value, name:'股票市值'}
+            {value: cash_use, name: '可用现金'},
+            {value: cash_freeze, name: '冻结金额'},
+            {value: stock_value, name: '股票市值'}
         ];
         var pie_div_id = document.getElementById('pie_canvas');
         draw_pie(parts, parts_value, pie_div_id); //绘制资金使用情况饼图
