7f5052b8be42c3cdc6cca9726a9fbf6d744ef2c5
tangchaohong <444092487@qq.com>
2015-11-19 10:52:30 +0800

    update

--- application/views/stocks/simu_performance.php
+++ application/views/stocks/simu_performance.php
@@ -36,55 +36,5 @@ $data['performance'] = $performance;
 </div>
 <?php $this->load->view('./templates/footer'); ?>
 </body>
-<script>
-    $(document).ready(function () {
-        //将数据显示格式化
-        $('.formatted').each(function () {
-            var value = format_num($(this).html());
-            $(this).html(value);
-        });
-        //拥有.render类的元素，若大于0，设置为红色，若小于0，设置为绿色
-        $('.render').each(function () {
-            var value = $(this).html().indexOf('-');
-            if (value == -1) {
-                $(this).css('color', 'red');
-            } else {
-                $(this).css('color', 'green');
-            }
-        });
-        var user_rate = new Array();
-        var avg_rate = new Array();
-        var time_list = new Array();
-        <?php $count = 0;?>
-        <?php foreach($perform_info as $item):?>
-        <?php $day_rate = round(floatval($item['day_rate'])*100, 2);?>
-        user_rate[<?php echo $count;?>] = <?php echo $day_rate;?>;
-        time_list[<?php echo $count;?>] = "<?php echo explode(' ', $item['timestamp'])[0];?>";
-        <?php $count += 1;?>
-        <?php endforeach;?>
-
-        <?php $count = 0;?>
-        <?php foreach($perform_avg as $item):?>
-        <?php $avg_rate = round(floatval($item['day_rate'])*100, 2);?>
-        avg_rate[<?php echo $count;?>] = <?php echo $avg_rate;?>;
-        <?php $count += 1;?>
-        <?php endforeach;?>
-
-        draw(user_rate, avg_rate, time_list);//绘制收益率曲线图
-
-        var parts = ['可用现金', '冻结金额', '股票市值'];
-        var cash_use = decimal(parseFloat("<?php echo $basic_info['cash_use'];?>"));
-        var cash_freeze = decimal(parseFloat("<?php echo $basic_info['cash_freeze'];?>"));
-        var stock_value = decimal(parseFloat("<?php echo $stock_value;?>"));
-        var parts_value = [
-            {value:cash_use, name:'可用现金'},
-            {value:cash_freeze, name:'冻结金额'},
-            {value:stock_value, name:'股票市值'}
-        ];
-        draw_pie(parts, parts_value); //绘制资金使用情况饼图
-
-    });
-
-</script>
 
 </html>
