4d15703859f528a4dcf19ecddaa46f7835897c92
tangchaohong <444092487@qq.com>
2015-11-30 16:37:36 +0800

    change the navbar

--- application/views/templates/jumptron.php
+++ application/views/templates/jumptron.php
@@ -1,27 +1,83 @@
 <!--巨幕-->
-<div class="main_jumptron">
-    <div class="container">
-        <br/><br/><br/>
-        <div class="row">
-            <div class="col-md-3 col-sm-3">
-                <div class="logo_pic">
-<!--                    <img class="img-responsive" src="--><?php //echo base_url('/assets/images/zhuge0.png'); ?><!--" alt="Logo加载中...">-->
+<div class="main_jumptron ">
+    <div class="main_jumptron_img">
+        <div id="jumptron-carousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#jumptron-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#jumptron-carousel" data-slide-to="1"></li>
+                <li data-target="#jumptron-carousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img class="jumptron_img" src="<?php echo base_url('assets/images/jumptron/1.png'); ?>" alt="...">
+
+                    <div class="carousel-caption">
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="jumptron_img" src="<?php echo base_url('assets/images/jumptron/2.png'); ?>" alt="...">
+                    <div class="carousel-caption">
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="jumptron_img" src="<?php echo base_url('assets/images/jumptron/3.png'); ?>" alt="...">
+                    <div class="carousel-caption">
+                    </div>
                 </div>
             </div>
-            <div class="col-md-9 col-sm-9">
-                <div class="logo">
-<!--                    <img class="img-responsive" src="--><?php //echo base_url('/assets/images/slogan1.png'); ?><!--" alt="Logo加载中...">-->
-                </div>
-            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#jumptron-carousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#jumptron-carousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+    <div class="jumptron_logo">
+        <div class="jinbang_num">
+            <p id="jinbang_num" class="bang_num inline_div">
+                <span>2</span>
+                <span>0</span>
+                <span>5</span>
+                <span>5</span>
+                人
+            </p>
+            <p>正在参与海天模拟炒股</p>
+        </div>
+        <hr/>
+        <div class="join-contest">
+            <p><span class="take_in_num">555</span>人正在角逐金榜,<span class="take_in_num">1500</span>人正在角逐银榜</p>
+            <p>
+                <a href="#" class="inline_div take_in">
+                    <span class="glyphicon glyphicon-hand-right"></span> 马上参加
+                </a>
+            </p>
         </div>
     </div>
 </div>
 
-<style type="text/css">
-    .main_jumptron
-    {
-/*        background-image: url("*/<?php //echo base_url('assets/images/jumptron_background.jpg'); ?>/*");*/
-        background-image: url("<?php echo base_url('assets/images/b_1_1.png'); ?>");
-        height: 420px;
+<script>
+    function adjust_img() {
+        $('.jumptron_img').each(function () {
+            var cur = $(window).width();
+            $(this).width(cur);
+        });
     }
-</style>
+    window.onload = function () {
+        window.onresize = adjust_img;
+        adjust_img();
+    }
+</script>
+
+
+
+
+
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,75 +1,87 @@
 <!--导航栏-->
-<nav class="navbar navbar-default navbar-fixed-top" xmlns="http://www.w3.org/1999/html" id="navbar">
+<!--从session中获取用户基本信息-->
+<?php $login_state = $this->session->userdata('login_state'); ?>
+<?php $identity = $this->session->userdata('identity') == '0' ? 'user' : 'master'; ?>
+<?php $uuid = $this->session->userdata('uuid'); ?>
+<?php $signature = $this->session->userdata('signature'); ?>
+<?php $avatar = $this->session->userdata('face_pic');?>
+<header class="header">
     <div class="container">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="<?php echo base_url('/index.php/index') ?>">首页</a>
-            </div>
-
-            <div class="collapse navbar-collapse narbar_font_size">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">理财室</a></li>
-                    <li><a href="<?php echo base_url('/index.php/stock/index/web/1/1');?>">华山论剑</a></li>
-                    <li><a href="#">知识服务</a></li>
-                    <li><a href="#">论坛</a></li>
-                </ul>
-
-                <!--根据是否登陆导航条显示不同的内容，当前默认为未登录-->
-                <?php $login_state = $this->session->userdata('login_state'); ?>
-                <?php $identity = $this->session->userdata('identity') == '0' ? 'user' : 'master'; ?>
-                <?php $uuid = $this->session->userdata('uuid'); ?>
-                <?php $signature = $this->session->userdata('signature'); ?>
-                <?php if ($login_state == false): ?>
-                    <div class="navbar-form navbar-right ">
-                        <a href="<?php echo base_url('/index.php/login') ?>">
-                            <input class="btn btn-danger" type="button" id="login" value="登陆"/></a>
-                        <a href="<?php echo base_url('/index.php/register') ?>">
-                            <input class="btn btn-danger" type="button" id="register" value="注册"/></a>
-                    </div>
-
-                <?php else: ?>
-                    <!--登陆成功后导航条上显示的内容-->
-                    <ul class="nav navbar-nav navbar-right gen_font_size">
-                        <!--用户的个人主页-->
-                        <?php if ($identity == 'master'): ?>
-                            <li>
-                                <a id="sel_center" href="<?php echo base_url('index.php/home/load_home/web/' . $identity . '/' . $uuid . '/' . '1'); ?>"><span class="glyphicon glyphicon-home"></span> 我的主页</a>
-                            </li>
-                        <?php else: ?>
-                        <?php endif; ?>
-                        <!--个人空间-->
-                        <li><a id="sel_homepage" href="<?php echo base_url('index.php/my_center/index'); ?>"><span class="glyphicon glyphicon-user"></span> 个人中心</a></li>
-                        <!--显示用户名-->
-                        <li class="dropdown">
-                            <a id='navbar-username' href="#" class="dropdown-toggle" data-toggle="dropdown"
-                               role="button"
-                               aria-haspopup="true" aria-expanded="false">
-                                <?php echo $this->session->userdata('username'); ?><span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">我的私信</a></li>
-                                <li><a href="#">我的收藏</a></li>
-                                <li><a href="#">我的钱包</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">设置</a></li>
-                                <li><a href="<?php echo base_url("index.php/login/logout"); ?>">退出</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                <?php endif; ?>
-            </div>
+        <div class="navbar-header">
+            <a class="navbar-brand" href="<?php echo base_url('/index.php/index');?>">
+                <img src="<?php echo base_url('assets/images/logo/logo_0.png'); ?>"/>
+            </a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
         </div>
+        <nav class="collapse navbar-collapse pull-left" style="margin-top: 5px;">
+            <ul class="nav navbar-nav top-menu">
+                <li><a href="<?php echo base_url('/index.php/index');?>">首页</a></li>
+                <li><a href="#">问答</a></li>
+                <li><a href="<?php echo base_url('/index.php/stocks/simulation/index') ?>">华山论剑</a></li>
+                <li><a href="#">论坛</a></li>
+            </ul>
+        </nav>
+
+        <!--根据是否登陆导航条显示不同的内容，当前默认为未登录-->
+        <?php if ($login_state == false): ?>
+            <div class="navbar-form navbar-right navbar-login">
+                <a href="<?php echo base_url('/index.php/login') ?>" class="theme-color">
+                    登陆</a>
+                <a href="<?php echo base_url('/index.php/register') ?>" class="theme-color">
+                    注册</a>
+            </div>
+        <?php else: ?>
+            <!--登陆成功后导航条上显示的内容-->
+            <ul class="nav pull-right user-nav">
+                <li class="dropdown user-nav-dropdown user-img">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                       aria-haspopup="true" aria-expanded="false"><img class="img-responsive"
+                                                                       src="<?php echo site_url('/uploads/'.$basic_info['face_pic']); ?>" alt="我的头像"/></a>
+                    <ul class="dropdown-menu avatar-dropdown-menu">
+                        <li><a href="<?php echo base_url('index.php/home/load_home/web/' . $identity . '/' . $uuid . '/' . '1'); ?>"><span class="glyphicon glyphicon-home"></span> 我的主页</a></li>
+                        <li><a href="<?php echo base_url('index.php/my_center/index'); ?>"><span class="glyphicon glyphicon-education"></span> 个人中心</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-cog"></span> 账号设置</a></li>
+                        <li><a href="<?php echo base_url("index.php/login/logout"); ?>"><span class="glyphicon glyphicon-log-out"></span> 退出登陆</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown user-nav-dropdown user-msg">
+                    <a  href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                        aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-envelope hz-icon"></span> 消息</a>
+                    <ul class="dropdown-menu msg-dropdown-menu" style="width: 50px">
+                        <li><a href="#"><span class="glyphicon glyphicon-question-sign"></span> 问答</a></li>
+                        <li><a href="#"><span
+                                    class="glyphicon glyphicon-bullhorn"></span> 通知</a></li>
+                        <li><a href="#"><span
+                                    class="glyphicon glyphicon-comment"></span> 回复</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-level-up"></span> 私信</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a class="btn com add-in-btn"><span class="glyphicon glyphicon-plus"></span> 理财师入驻</a>
+                </li>
+            </ul>
+        <?php endif; ?>
     </div>
-</nav>
+</header>
+
 <script>
     $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
+        $('li.user-img').hover(function () {
+            $('.avatar-dropdown-menu').show();
+        }, function () {
+            $('.avatar-dropdown-menu').hide();
+        });
+        $('li.user-msg').hover(function () {
+            $('.msg-dropdown-menu').show();
+        }, function () {
+            $('.msg-dropdown-menu').hide();
+        });
     })
 </script>
-
