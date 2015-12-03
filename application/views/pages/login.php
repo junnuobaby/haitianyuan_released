8b3d0959b1c8955c7fe0d392c8cef372a65acf9d
tangchaohong <444092487@qq.com>
2015-12-03 11:56:51 +0800

    update the login and register page

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,39 +1,29 @@
-<!--登陆-->
-<?php
+<!--登陆--><?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="zh-cn">
 <?php $this->load->view('./templates/head'); ?>
 <body>
 <div class="wrapper">
-    <?php $this->load->view('./templates/navbar'); ?>
-    <!--海天登陆-->
-    <div class="login-box <?php if ($nav_mode == 'register') {
+    <?php $this->load->view('./templates/navbar'); ?><!--海天登陆-->
+    <div class="login-box <?php if($nav_mode == 'register') {
         echo 'register';
     } ?>">
         <div class="login-form ">
-            <h1>登陆海天
-                <a href="#" class="pull-right turn-to-regist turn-btn">注册 <span
-                        class="glyphicon glyphicon-hand-right"></span></a>
-            </h1>
-            <?php $username_error = form_error('user_name_input'); ?>
+            <h1>登陆海天 <a href="#" class="pull-right turn-to-regist turn-btn">注册
+                    <span class="glyphicon glyphicon-hand-right"></span></a>
+            </h1><?php $username_error = form_error('user_name_input'); ?>
             <?php $password_error = form_error('password_input'); ?>
             <?php echo form_open('login/login', 'class="form-horizontal login_form"'); ?>
             <div class="input-group <?php echo $username_error ? 'has-error' : ''; ?>">
-                    <span class="input-group-addon"><span
-                            class="glyphicon glyphicon-user"></span></span>
-                <input type="text" class="form-control" id="user_name_input"
-                       name="user_name_input"
-                       placeholder="<?php if($username_error) echo $username_error; else echo '手机号/会员名/邮箱'?>">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                <input type="text" class="form-control" id="user_name_input" name="user_name_input" placeholder="<?php if($username_error) echo $username_error; else echo '手机号/会员名/邮箱' ?>">
             </div>
             <p class="theme-color hidden">请输入账号</p>
 
             <div class="input-group <?php echo $password_error ? 'has-error' : ''; ?>">
-                    <span class="input-group-addon"><span
-                            class="glyphicon glyphicon-lock"></span></span>
-                <input type="password" class="form-control" id="password_input"
-                       name="password_input"
-                       placeholder="<?php if($password_error) echo $password_error; else echo '密码'?>">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                <input type="password" class="form-control" id="password_input" name="password_input" placeholder="<?php if($password_error) echo $password_error; else echo '密码' ?>">
             </div>
             <p class="theme-color hidden">请输入密码</p>
 
@@ -58,72 +48,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <ul class="nav navbar-nav txt-center">
                 <!--qq第三方登陆-->
                 <li><a href="#" class="white-color">
-                        <img width="20px" height="20px" src="<?php echo base_url('/assets/images/qq.png'); ?>"
-                             alt="..." class="img-rounded narbar_li"> qq登陆</a></li>
+                        <img width="20px" height="20px" src="<?php echo base_url('/assets/images/qq.png'); ?>" alt="..." class="img-rounded narbar_li"> qq登陆</a>
+                </li>
                 <!--微博第三方登陆-->
                 <li><a href="#" class="white-color">
-                        <img width="20px" height="20px" src="<?php echo base_url('/assets/images/weibo.png'); ?>"
-                             alt="..." class="img-rounded narbar_li"> 微博登陆</a></li>
+                        <img width="20px" height="20px" src="<?php echo base_url('/assets/images/weibo.png'); ?>" alt="..." class="img-rounded narbar_li"> 微博登陆</a>
+                </li>
             </ul>
         </div>
     </div>
     <!--  海天注册-->
-    <div class="login-box <?php if ($nav_mode == 'login') {
+    <div class="login-box <?php if($nav_mode == 'login') {
         echo 'register';
     } ?>">
         <div class="login-form register-form">
             <h1>注册账户
-                <a href="#" class="pull-right turn-btn turn-to-login">登陆 <span
-                        class="glyphicon glyphicon-hand-right"></span></a>
-            </h1>
-            <?php $phone_error = form_error('phone_number'); ?>
+                <a href="#" class="pull-right turn-btn turn-to-login">登陆<span class="glyphicon glyphicon-hand-right"></span></a>
+            </h1><?php $phone_error = form_error('phone_number'); ?>
             <?php $code_error = form_error('phone_code'); ?>
             <?php $nickname_error = form_error('nick_name'); ?>
             <?php $pwd_error = form_error('password'); ?>
             <?php echo form_open('register/load_info', 'class="form-horizontal login_form"') ?>
             <div class="input-group <?php echo $phone_error ? 'has-error' : ''; ?>">
-                    <span class="input-group-addon">
-                        <span class="glyphicon glyphicon-earphone"></span>
-                    </span>
-                <input type="tel" class="form-control" id="user_mobile" name="phone_number"
-                       value="<?php echo !$phone_error ? set_value('phone_number') : ''; ?>"
-                       placeholder="<?php if($phone_error) echo $phone_error; else echo '手机号'?>">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-earphone"></span></span>
+                <input type="tel" class="form-control" id="user_mobile" name="phone_number" value="<?php echo !$phone_error ? set_value('phone_number') : ''; ?>" placeholder="<?php if($phone_error) echo $phone_error; else echo '手机号' ?>">
             </div>
             <p class="theme-color hidden" id="user_mobile_error"></p>
-
             <div class="input-group <?php echo $code_error ? 'has-error' : ''; ?>">
-                    <span class="input-group-addon"><span
-                            class="glyphicon glyphicon-eye-open"></span></span>
-                <input type="text" class="form-control" id="verification_code" name="phone_code"
-                       value="<?php echo !$code_error ? set_value('phone_code') : ''; ?>"
-                       placeholder="<?php if($code_error) echo $code_error; else echo '验证码'?>">
-
-                <div class="input-group-btn">
-                    <button id="send_code" type="button" class="btn self-btn-danger" disabled="disabled">获取验证码</button>
-                </div>
+                <span class="input-group-addon"><span class="glyphicon glyphicon-eye-open"></span></span>
+                <input type="text" class="form-control" id="verification_code" name="phone_code" value="<?php echo !$code_error ? set_value('phone_code') : ''; ?>" placeholder="<?php if($code_error) echo $code_error; else echo '验证码' ?>">
+                <div class="input-group-btn"><button id="send_code" type="button" class="btn self-btn-danger" disabled="disabled">获取验证码</button></div>
             </div>
             <p class="theme-color hidden nick_name_error" id="ver_code_error"></p>
-
             <div class="input-group <?php echo $nickname_error ? 'has-error' : ''; ?>">
-                    <span class="input-group-addon"><span
-                            class="glyphicon glyphicon-user"></span></span>
-                <input type="text" class="form-control" id="nick_name"
-                       name="nick_name" required
-                       value="<?php echo !$nickname_error ? set_value('nick_name') : ''; ?>"
-                       placeholder="<?php if($nickname_error) echo $nickname_error; else echo '请输入昵称'?>">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                <input type="text" class="form-control" id="nick_name" name="nick_name" required value="<?php echo !$nickname_error ? set_value('nick_name') : ''; ?>" placeholder="<?php if($nickname_error) echo $nickname_error; else echo '请输入昵称' ?>">
             </div>
             <p class="theme-color hidden nick_name_error" id="nick_name_error"></p>
-
             <div class="input-group <?php echo $pwd_error ? 'has-error' : ''; ?>">
-                    <span class="input-group-addon"><span
-                            class="glyphicon glyphicon-lock"></span></span>
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
                 <input type="password" style="display:none">
-                <input type="password" class="form-control" id="password"
-                       name="password"
-                       placeholder="<?php if($pwd_error) echo $pwd_error; else echo '密码 (不少于6位)'?>">
+                <input type="password" class="form-control" id="password" name="password" placeholder="<?php if($pwd_error) echo $pwd_error; else echo '密码 (不少于6位)' ?>">
             </div>
             <p class="theme-color hidden" id="pwd_error"></p>
-
             <div class="row pwd-help-info">
                 <div class="col-md-12">
                     <div class="login-btn-div">
@@ -136,18 +103,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="social-login">
             <ul class="nav navbar-nav txt-center">
                 <!--qq第三方登陆-->
-                <li><a href="#" class="white-color">
-                        <img width="20px" height="20px" src="<?php echo base_url('/assets/images/qq.png'); ?>"
-                             alt="..." class="img-rounded narbar_li"> qq登陆</a></li>
+                <li>
+                    <a href="#" class="white-color">
+                        <img width="20px" height="20px" src="<?php echo base_url('/assets/images/qq.png'); ?>" alt="..." class="img-rounded narbar_li"> qq登陆
+                    </a>
+                </li>
                 <!--微博第三方登陆-->
-                <li><a href="#" class="white-color">
-                        <img width="20px" height="20px" src="<?php echo base_url('/assets/images/weibo.png'); ?>"
-                             alt="..." class="img-rounded narbar_li"> 微博登陆</a></li>
+                <li>
+                    <a href="#" class="white-color">
+                        <img width="20px" height="20px" src="<?php echo base_url('/assets/images/weibo.png'); ?>" alt="..." class="img-rounded narbar_li"> 微博登陆
+                    </a>
+                </li>
             </ul>
         </div>
     </div>
 </div>
-<!--.wrapper-->
 <?php $this->load->view('./templates/footer'); ?>
 </body>
 </html>
@@ -158,11 +128,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     }
 </style>
 <script type="text/javascript">
-    //    倒计时一分钟
+    /**
+     * 全局变量
+     * second - 倒计时设置为59s
+     * speed - 每次计数间隔1s
+     * send_code - 获取验证码按钮
+     * count_down - 默认非倒计时状态
+     */
     var second = 59;
     var speed = 1000;
     var send_code = $('#send_code');
     var count_down = false;
+
+    /**
+     * 倒计时一分钟
+     * seconds - 倒计时秒数
+     * speed - 每次计数间隔
+     */
     function countDown(seconds, speed) {
         var txt = '倒计时 ' + ((seconds < 10) ? "0" + seconds : seconds) + ' 秒';
         send_code.html(txt);
@@ -177,13 +159,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             count_down = false;
         }
     }
-
     $(document).ready(function () {
         var user_mobile = $('#user_mobile');
         var nick_name = $('#nick_name');
         var password = $('#password');
         var validate_code = $('#verification_code');
-        //发送手机验证码
+
+        /**
+         * 发送手机验证码
+         * count_down - 是否已经倒计时
+         */
         send_code.click(function () {
             if (!count_down) {
                 var xmlhttp = new XMLHttpRequest();
@@ -193,7 +178,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 countDown(second, speed);
             }
         });
-        //检查用户输入的手机号码是否已经被注册
+
+        /**
+         * 验证手机号是否已注册
+         */
         user_mobile.blur(function () {
                 var phone_num = $.trim($('#user_mobile').val());
                 if (validate_phone(phone_num)) {
@@ -201,7 +189,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         function (data) {
                             if (data == 'true') {
                                 $('#user_mobile_error').removeClass('hidden').html('(该号码已注册！)');
-                            }else{
+                            } else {
                                 $('#user_mobile_error').addClass('hidden');
                             }
                         });
@@ -209,7 +197,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 }
             }
         );
-        //检查验证码是否为6位纯数字
+        user_mobile.focus(function () {
+            $('#user_mobile_error').addClass('hidden');
+        });
+
+        /**
+         * 验证码是否为6位纯数字
+         */
         validate_code.blur(function () {
             var value = $.trim(validate_code.val());
             var legal = /^\d+$/.test(value);
@@ -219,11 +213,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             else {
                 $('#ver_code_error').addClass('hidden');
             }
-
         });
-        //检查昵称是否已存在
-        nick_name.blur(
-            function () {
+        validate_code.onfocus(function () {
+            $('#ver_code_error').addClass('hidden');
+        });
+
+        /**
+         * 检查昵称是否已存在
+         */
+        nick_name.blur(function () {
                 var nick_name_value = $.trim($('#nick_name').val());
                 if (validate_nick_name(nick_name_value)) {
                     $.get("<?php echo base_url('/index.php/register/is_exist/web/')?>" + '/' + nick_name_value,
@@ -231,14 +229,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             if (data == 'true') {
                                 $('#nick_name_error').removeClass('hidden').html('(该昵称已存在！)');
                             }
-                            else{
+                            else {
                                 $('#nick_name_error').addClass('hidden');
                             }
                         });
                 }
             }
         );
-        //检查密码格式,长度6~32位
+        nick_name.focus(function () {
+            $('#nick_name_error').addClass('hidden');
+        });
+
+        /**
+         * 检查密码格式,长度6~32位
+         * legal - 密码是否为纯数字
+         */
         password.blur(function () {
             var value = $.trim(password.val());
             var legal = /^\d+$/.test(value);
@@ -248,28 +253,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 $('#pwd_error').addClass('hidden');
             }
         });
+        password.focus(function () {
+            $('#pwd_error').addClass('hidden');
+        });
     });
-    //检查手机号码的合法性，长度为11位
+
+    /**
+     *  检查手机号码的合法性，长度为11位
+     *  value - 手机号
+     */
     function validate_phone(value) {
-            var re = /1[3458]{1}\d{9}$/;
-            var legal = re.test(value);
-            if (value.length != 11 || !legal) {
-                $('#user_mobile_error').removeClass('hidden').html('请输入11位有效手机号码！');
-                $('#send_code').attr("disabled", "disabled");
-                return false;
-            }
-            else {
-                $('#user_mobile_error').addClass('hidden');
-                return true;
-            }
+        var re = /1[3458]{1}\d{9}$/;
+        var legal = re.test(value);
+        if (value.length != 11 || !legal) {
+            $('#user_mobile_error').removeClass('hidden').html('请输入11位有效手机号码！');
+            $('#send_code').attr("disabled", "disabled");
+            return false;
+        }
+        else {
+            $('#user_mobile_error').addClass('hidden');
+            return true;
+        }
     }
-    //昵称格式验证，不能全为数字
+
+    /**
+     * 昵称格式验证，不能全为数字，不能为空
+     * value - 用户昵称
+     */
     function validate_nick_name(value) {
         if (/^\d+$/.test(value)) {
             $('#nick_name_error').removeClass('hidden').html('(昵称不能为纯数字！)');
             return false;
         }
-        if(value.length < 1){
+        if (value.length < 1) {
             $('#nick_name_error').removeClass('hidden').html('(昵称不能为空！)');
             return false;
         }
@@ -279,7 +295,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         }
     }
 
-    //控制登陆界面的动画
+    /**
+     * 登陆注册界面的动画切换
+     */
     $(document).ready(function () {
         var login_box = $('div.login-box');
         $('a.turn-btn').click(function () {
