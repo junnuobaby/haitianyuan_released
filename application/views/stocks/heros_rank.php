900efb599c62edd63501d98a4ce185653ae283b7
tangchaohong <444092487@qq.com>
2015-12-04 16:54:17 +0800

    update the stocks

--- application/views/stocks/heros_rank.php
+++ application/views/stocks/heros_rank.php
@@ -44,9 +44,9 @@ $ranking = 0;
                     <div class="rank_table">
                         <div class="col-md-8 col-md-offset-4">
                             <div class="plaque">
-                                <img width="70px" height="70px" class="img-responsive "
+                                <img width="70px" height="70px" class="img-responsive inline_block"
                                      src="<?php echo base_url('/assets/images/icon/chanshi.png'); ?>" alt="Logo加载中...">
-                                <img class="img-responsive "
+                                <img class="img-responsive inline_block "
                                      src="<?php echo base_url('/assets/images/icon/chanshilogo.png'); ?>"
                                      alt="Logo加载中...">
                             </div>
