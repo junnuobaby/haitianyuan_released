64da01da66c401aab3d411122e3c566de38026f6
tangchaohong <444092487@qq.com>
2015-12-04 17:02:44 +0800

    update the stocks

--- application/views/stocks/simu_cancel.php
+++ application/views/stocks/simu_cancel.php
@@ -19,7 +19,7 @@ $pages = $pre_list['pagination']; //获取分页
                 <div class="simulate_panel">
                     <div class="tab-content">
                         <h4 class="theme-color container_to_top">委托单</h4>
-                        <div class="panel panel-default">
+                        <div class="panel panel-default border_none">
                             <div class="panel-body">
                                 <table class="table table-bordered table-responsive table-hover table-striped ">
                                     <thead>
