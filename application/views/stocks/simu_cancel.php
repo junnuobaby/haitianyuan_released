b4eba3b75134aa77ee3b62433fd2accdee7a7d64
tangchaohong <444092487@qq.com>
2015-12-06 16:24:48 +0800

    update the stocks

--- application/views/stocks/simu_cancel.php
+++ application/views/stocks/simu_cancel.php
@@ -1,12 +1,31 @@
-<!--模拟炒股，委托单--><?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!--模拟炒股，委托单-->
+<?phpdefined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
 <html lang="zh-cn">
-<?php $this->load->view('./templates/head'); ?>
 <?php
+/**
+ * records - dict - 还在委托状态的订单详情
+ *           key:
+ *              pre_id - 订单编号
+ *              trade_type - 操作（0 - 买入 2- 卖出）
+ *              SecurityID - 代码
+ *              Symbol - 名称
+ *              Volume - 挂单数量
+ *              price_order - 挂单价格
+ *              price_full - 挂单全价
+ *              fund_deal - 挂单金额
+ *              fee - 手续费
+ *              tax - 印花税
+ *              other_fee - 其他杂费
+ *              hap_fund - 发送金额
+ *              remain_fund - 现金余额
+ *              timestamp - 下单时间
+ *              tip - 备注
+ */
 $records = $pre_list['data_page']; //获取还在委托状态的订单详情
 $pages = $pre_list['pagination']; //获取分页
 ?>
+<?php $this->load->view('./templates/head'); ?>
 <body class="bg-gray">
 <div class="wrapper">
     <?php $this->load->view('./stocks/bonds_navbar'); ?>
@@ -15,69 +34,72 @@ $pages = $pre_list['pagination']; //获取分页
             <?php $this->load->view('./stocks/simu_menu'); ?>
         </div>
         <div class="row">
-            <div class="bg-white stocks_min_h  block-radius">
-                <div class="simulate_panel">
-                    <div class="tab-content">
-                        <h4 class="theme-color container_to_top">委托单</h4>
-                        <div class="panel panel-default border_none">
-                            <div class="panel-body">
-                                <table class="table table-bordered table-hover table-striped ">
-                                    <thead>
-                                    <tr>
-                                        <th>订单号</th>
-                                        <th>操作</th>
-                                        <th>证券代码</th>
-                                        <th>证券名称</th>
-                                        <th>挂单数量</th>
-                                        <th>挂单价格</th>
-                                        <th>挂单全价</th>
-                                        <th>挂单金额</th>
-                                        <th>手续费</th>
-                                        <th>印花税</th>
-                                        <th>其他杂费</th>
-                                        <th>发生金额</th>
-                                        <th>现金余额</th>
-                                        <th>下单时间</th>
-                                        <th>备注</th>
-                                        <th>撤单</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <?php foreach ($records as $stock_item): ?>
-                                        <tr class="done_record">
-                                            <td><?php echo $stock_item['pre_id']; ?></td>
+            <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
+                <div class="bg-white stocks_min_h  block-radius">
+                    <div class="simulate_panel">
+                        <div class="tab-content">
+                            <h4 class="theme-color container_to_top">委托单</h4>
+                            <div class="panel panel-default border_none">
+                                <div class="panel-body">
+                                    <table class="table table-bordered table-hover table-striped ">
+                                        <thead>
+                                        <tr>
+    <!--                                        <th>编号</th>-->
+                                            <th>代码</th>
+                                            <th>名称</th>
+                                            <th>操作</th>
+                                            <th>委托数量</th>
+                                            <th>委托价格</th>
+<!--                                            <th>挂单全价</th>-->
+                                            <th>金额</th>
+<!--                                            <th>手续费</th>-->
+<!--                                            <th>印花税</th>-->
+<!--                                            <th>其他杂费</th>-->
+<!--                                            <th>发生金额</th>-->
+<!--                                            <th>现金余额</th>-->
+                                            <th>委托时间</th>
+                                            <th>备注</th>
+                                            <th>撤单</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php foreach ($records as $stock_item): ?>
+                                            <tr class="done_record">
+    <!--                                            <td>--><?php //echo $stock_item['pre_id']; ?><!--</td>-->
+
+                                                <td data-toggle="modal" data-target="#graphModal" onclick="fillimage('<?= $stock_item['SecurityID']; ?>', '<?= $stock_item['Symbol']; ?>')">
+                                                    <a href="#" class="hty_a"><?php echo $stock_item['SecurityID']; ?></a>
+                                                </td>
+                                                <td><?php echo $stock_item['Symbol']; ?></td>
                                             <td><?php if($stock_item['trade_type'] == '0') {
                                                     echo '买入';
                                                 } else if($stock_item['trade_type'] == '2') {
                                                     echo "卖出";
                                                 } ?></td>
-                                            <td data-toggle="modal" data-target="#graphModal" onclick="fillimage('<?= $stock_item['SecurityID']; ?>', '<?= $stock_item['Symbol']; ?>')">
-                                                <a href="#" class="hty_a"><?php echo $stock_item['SecurityID']; ?></a>
-                                            </td>
-                                            <td><?php echo $stock_item['Symbol']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['Volume']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['price_order']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['price_full']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['fund_deal']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['fee']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['tax']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['other_fee']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['hap_fund']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['remain_fund']; ?></td>
-                                            <td><?php echo $stock_item['timestamp']; ?></td>
-                                            <td><?php echo $stock_item['tip']; ?></td>
-                                            <td>
-                                                <a href="#" class="theme-color cancel_btn" data-id="<?php echo $stock_item['pre_id']; ?>">撤单</a>
-                                            </td>
-                                        </tr><?php endforeach; ?>
-                                    </tbody>
-                                </table>
+                                                <td class="formatted"><?php echo $stock_item['Volume']; ?></td>
+                                                <td class="formatted"><?php echo $stock_item['price_order']; ?></td>
+<!--                                                <td class="formatted">--><?php //echo $stock_item['price_full']; ?><!--</td>-->
+                                                <td class="formatted"><?php echo $stock_item['fund_deal']; ?></td>
+<!--                                                <td class="formatted">--><?php //echo $stock_item['fee']; ?><!--</td>-->
+<!--                                                <td class="formatted">--><?php //echo $stock_item['tax']; ?><!--</td>-->
+<!--                                                <td class="formatted">--><?php //echo $stock_item['other_fee']; ?><!--</td>-->
+<!--                                                <td class="formatted">--><?php //echo $stock_item['hap_fund']; ?><!--</td>-->
+<!--                                                <td class="formatted">--><?php //echo $stock_item['remain_fund']; ?><!--</td>-->
+                                                <td><?php echo $stock_item['timestamp']; ?></td>
+                                                <td><?php echo $stock_item['tip']; ?></td>
+                                                <td>
+                                                    <a href="#" class="theme-color cancel_btn" data-id="<?php echo $stock_item['pre_id']; ?>">撤单</a>
+                                                </td>
+                                            </tr><?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
+                            <div class="text-center"><p class="pages"><?php echo $pages; ?></p></div>
                         </div>
-                        <div class="text-center"><p class="pages"><?php echo $pages; ?></p></div>
                     </div>
                 </div>
-            </div>
+            <div>
         </div>
     </div>
 
