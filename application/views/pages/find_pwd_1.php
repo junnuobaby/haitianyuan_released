260ab9b27bd489875025598e5868cfe47a8e4aaf
tangchaohong <444092487@qq.com>
2015-12-14 17:07:34 +0800

    update the login page

--- application/views/pages/find_pwd_1.php
+++ application/views/pages/find_pwd_1.php
@@ -31,12 +31,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </div>
                         <div class="form-group">
                             <label class="col-sm-3 control-label"></label>
-                            <div class="col-sm-3">
-<!--                                <img class="img_vcode" src="--><?php //echo base_url('index.php/findpwd/update_captcha/'); ?><!--"/>-->
-                                <div id="vcode_img" style="border: 1px solid black;">
-
-                                </div>
-<!--                                --><?php //echo base_url('index.php/findpwd/update_captcha/'); ?>
+                            <div class="col-sm-5">
+                                <div id="vcode_img" style="border: 1px solid black;"></div>
                             </div>
                             <a href="#" class="next_vcode">看不清，换一张</a>
                         </div>
