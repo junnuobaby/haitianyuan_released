b8ea75a720a7924bb2ef55572e0317ca0d1035c3
tangchaohong <444092487@qq.com>
2015-12-15 11:14:18 +0800

    update the login page

--- application/views/pages/find_pwd_3.php
+++ application/views/pages/find_pwd_3.php
@@ -22,12 +22,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <label for="new_pwd" class="col-sm-3 control-label">新密码</label>
                             <div class="col-sm-5">
                                 <input type="password" class="form-control" id="new_pwd" name="new_pwd" placeholder="<?php if($new_pwd_error) echo $new_pwd_error; else echo '请输入不少于6位的密码，不能为纯数字'; ?>">
+                                <span class="new_pwd_error"></span>
                             </div>
                         </div>
                         <div class="form-group <?php echo $new_pwd_again_error ? 'has-error' : ''; ?>">
                             <label for="new_pwd_agin" class="col-sm-3 control-label">确认密码</label>
                             <div class="col-sm-5">
                                 <input type="password" class="form-control" id="new_pwd_agin" name="new_pwd_again" placeholder="<?php if($new_pwd_again_error) echo $new_pwd_again_error; else echo '请再次输入新密码'; ?>">
+                                <span class="new_pwd_again_error"></span>
                             </div>
                         </div>
                         <div class="form-group">
@@ -60,15 +62,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </html>
 <script>
     $(document).ready(function () {
+        $('input[type="password"]').focus(function () {
+            $('.new_pwd_error').html();
+            $('.new_pwd_again_error').html();
+        });
         $('#reset_pwd_btn').click(function () {
             var pwd = $('#new_pwd');
             var pwd_value = $.trim(pwd.val());
             var pwd_again = $('#new_pwd_again');
             var pwd_again_value = $.trim(pwd_again.val());
             if(pwd_value.length < 6 || /^\d+$/.test(pwd_value)){
-                pwd.attr('placeholder','密码不能少于6位，且不能为纯数字');
+                $('.new_pwd_error').html('密码不能少于6位，且不能为纯数字');
             }else if(pwd_value !== pwd_again_value) {
-                pwd_again.attr('placeholder', '两次密码不一致');
+                $('.new_pwd_again_error').html('两次密码不一致');
             }
             else{
                 $('#reset_pwd_form').submit();
