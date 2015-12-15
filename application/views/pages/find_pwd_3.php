e9236481f837c953a98ab71735c4dfd441d7aee6
tangchaohong <444092487@qq.com>
2015-12-15 15:37:02 +0800

    update the login page

--- application/views/pages/find_pwd_3.php
+++ application/views/pages/find_pwd_3.php
@@ -22,14 +22,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <label for="new_pwd" class="col-sm-3 control-label">新密码</label>
                             <div class="col-sm-5">
                                 <input type="password" class="form-control" id="new_pwd" name="new_pwd" placeholder="<?php if($new_pwd_error) echo $new_pwd_error; else echo '请输入不少于6位的密码，不能为纯数字'; ?>">
-                                <span class="new_pwd_error"></span>
+                                <span class="new_pwd_error text-danger"></span>
                             </div>
                         </div>
                         <div class="form-group <?php echo $new_pwd_again_error ? 'has-error' : ''; ?>">
                             <label for="new_pwd_agin" class="col-sm-3 control-label">确认密码</label>
                             <div class="col-sm-5">
                                 <input type="password" class="form-control" id="new_pwd_again" name="new_pwd_again" placeholder="<?php if($new_pwd_again_error) echo $new_pwd_again_error; else echo '请再次输入新密码'; ?>">
-                                <span class="new_pwd_again_error"></span>
+                                <span class="new_pwd_again_error text-danger"></span>
                             </div>
                         </div>
                         <div class="form-group">
