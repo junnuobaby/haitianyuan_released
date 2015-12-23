ee8e943e51d375f3671275c181130095ca4ce5d6
tangchaohong <444092487@qq.com>
2015-12-23 17:58:39 +0800

    update the login page

--- application/views/stocks/heros_rank.php
+++ application/views/stocks/heros_rank.php
@@ -81,8 +81,7 @@ $ranking = 0;
                                             ?>
                                             <?= $ranking; ?></td>
                                         <td class="table_left">
-                                            <img width="20px" height="20px" src="<?php echo base_url('/uploads/' . $item['face_pic']); ?>"
-                                                class="img-responsive " alt="..."> <?= $item['user_name']; ?></td>
+                                            <img width="20px" height="20px" src="<?php echo base_url('/uploads/' . $item['face_pic']); ?>" alt="..."> <?= $item['user_name']; ?></td>
                                         <td class="formatted table_right"><?= $item['fund']; ?></td>
                                         <td class="table_right"><?= number_format(floatval($item['position']) * 100, 2); ?>
                                             %
