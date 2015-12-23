a8d0e77381de72a8ea766514e800ba051a6c4c53
tangchaohong <444092487@qq.com>
2015-12-23 18:30:48 +0800

    update the login page

--- application/views/stocks/heros_rank.php
+++ application/views/stocks/heros_rank.php
@@ -32,10 +32,10 @@ $ranking = 0;
                 <div id="ranking_sidebar" class="flow-sidebar" data-spy="affix" data-offset-top='70'>
                     <div class="list-group">
                         <a href="#">海天金榜</a>
-                        <a href="<?php echo base_url('/index.php/stock/index/web/2/4');?>" class="list-group-item <?php if($nav_mode == "simu_heros_5") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
-                        <a href="<?php echo base_url('/index.php/stock/index/web/2/4');?>" class="list-group-item <?php if($nav_mode == "simu_heros_6") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
-                        <a href="<?php echo base_url('/index.php/stock/index/web/2/4');?>" class="list-group-item <?php if($nav_mode == "simu_heros_7") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
-                        <a href="<?php echo base_url('/index.php/stock/index/web/2/4');?>" class="list-group-item <?php if($nav_mode == "simu_heros_8") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
+                        <a href="<?php echo base_url('/index.php/stock/index/web/2/5');?>" class="list-group-item <?php if($nav_mode == "simu_heros_5") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
+                        <a href="<?php echo base_url('/index.php/stock/index/web/2/6');?>" class="list-group-item <?php if($nav_mode == "simu_heros_6") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
+                        <a href="<?php echo base_url('/index.php/stock/index/web/2/7');?>" class="list-group-item <?php if($nav_mode == "simu_heros_7") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
+                        <a href="<?php echo base_url('/index.php/stock/index/web/2/8');?>" class="list-group-item <?php if($nav_mode == "simu_heros_8") echo "active"; ?>"><span class="glyphicon glyphicon-th"></span> 月收益率排名</a>
                         <a href="#">海天银榜</a>
                         <a href="<?php echo base_url('/index.php/stock/index/web/2/1');?>" class="list-group-item <?php if($nav_mode == "simu_heros_1") echo "active"; ?>"><span class="glyphicon glyphicon-star"></span> 总收益率排名</a>
                         <a href="<?php echo base_url('/index.php/stock/index/web/2/2');?>" class="list-group-item <?php if($nav_mode == "simu_heros_2") echo "active"; ?>"><span class="glyphicon glyphicon-asterisk"></span> 日收益率排名</a>
