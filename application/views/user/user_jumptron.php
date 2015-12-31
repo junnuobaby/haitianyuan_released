3a8dfa4ae07fc836228b4028694295bacf46cb85
tangchaohong <444092487@qq.com>
2015-12-31 18:11:13 +0800

    update the login page

--- application/views/user/user_jumptron.php
+++ application/views/user/user_jumptron.php
@@ -8,7 +8,7 @@ $face_pic = $info['face_pic'];
     <div class="container">
         <div><img class="img-responsive user_avatar_box img-circle" src="<?php echo base_url('/uploads/' . $face_pic); ?>" alt="用户头像"></div>
         <div class="user_homepage_name"><?php echo $username; ?></div>
-        <p class="user_signature"><?php echo $signature; ?></p>
+        <p class="user_signature"><?php if(count($signature) < 1) echo '暂未设置个性签名'; else echo $signature; ?></p>
     </div>
 </div>
 <script>
