6a40f8e2204616304e1033b47aa37b422796f1b4
tangchaohong <444092487@qq.com>
2016-01-04 14:30:21 +0800

    update the login page

--- application/views/user/user_homepage.php
+++ application/views/user/user_homepage.php
@@ -31,7 +31,7 @@ $user_id = $info['host_id'];
                                         <span class="q_a_span">问</span>
                                         <a href="#"><?php echo $qa_item['qu_content']; ?> </a></h4>
                                     <div class="qu_time">
-                                        <span><?php echo $qa_item['qu_timestamp'] ?></span>来自<a href="<?php echo base_url('index.php/home/load_home/web/' . 'user' . '/' . $qa_item['user_id'] . '/' . '1'); ?>" class="questioner"><?php echo $qa_item['questioner'] ?></a>
+                                        <span><?php echo $qa_item['qu_timestamp'] ?></span>向<a href="<?php echo base_url('index.php/home/load_home/web/' . 'master' . '/' . $qa_item['master_id'] . '/' . '1'); ?>" class="questioner"><?php echo $qa_item['answerer'];?>提问</a>
                                     <span class="key_word"><?php if ($qa_item['kwords'] == '') {
                                             echo '暂无标签';
                                         } else {
