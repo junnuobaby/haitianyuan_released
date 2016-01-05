0f7c039c52961de7cf080c32bfa7127a262f7d7d
tangchaohong <444092487@qq.com>
2016-01-05 10:40:26 +0800

    update the login page

--- application/views/master/master_simulation.php
+++ application/views/master/master_simulation.php
@@ -159,9 +159,9 @@ $fpl_rate = $stock['pl_rate'];
                                             <td class="formatted"><?php echo intval($stock_item['Volume_All']); ?></td>
                                             <td class="formatted"><?php echo $sell_avail;?></td>
                                             <td><?php echo number_format(floatval($stock_item['BuyCost']), 2); ?></td>
-                                            <td class="formatted"><?php echo number_format(floatval($stock_item['TradePrice']), 2); ?></td>
+                                            <td class="formatted"><?php echo number_format(floatval($stock_item['TradePrice']), 0); ?></td>
                                             <td class="formatted"><?php echo $stock_item['float_pl']; ?></td>
-                                            <td class="formatted"><?php echo $stock_item['float_pl_rate']; ?></td>
+                                            <td class="stock_pl_rate"><?php echo $stock_item['float_pl_rate']; ?>%</td>
                                         </tr>
                                     <?php endforeach; ?>
                                     </tbody>
