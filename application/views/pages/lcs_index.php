75b250662e9a7f7f6864aa5bf30f152b1d11fc06
tangchaohong <444092487@qq.com>
2016-01-06 10:32:37 +0800

    update the login page

--- application/views/pages/lcs_index.php
+++ application/views/pages/lcs_index.php
@@ -124,7 +124,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         method: 'get',
                         success: function (data) {
                             if (data.status == '0') {
-                                alert($(this).html());
+                                alert(data.status);
                                 $(this).html('关注');
                             } else {
                                 alert(data.msg);
