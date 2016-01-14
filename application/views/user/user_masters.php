0677fe22e3b716ee9308169ee875aec3e5abe137
tangchaohong <444092487@qq.com>
2016-01-14 12:13:02 +0800

    update the index

--- application/views/user/user_masters.php
+++ application/views/user/user_masters.php
@@ -34,7 +34,7 @@ $vips = $master_info;
                                         </div>
                                     </div>
                                     <div class="col-md-9">
-                                        <h4><a href= "<?php echo base_url('index.php/home/load_home/web/' . 'master' . '/' . $vip['user_id'] . '/' . '1'); ?>" class="theme-color"><?php echo $vip['master_name']; ?></a></h4>
+                                        <h4><a href= "<?php echo base_url('index.php/home/load_home/web/' . 'master' . '/' . $vip['user_id'] . '/' . '1'); ?>" class="theme-color"><?php echo $vip['user_name']; ?></a></h4>
                                         <p>关注 <?php echo $vip['concerns_count']; ?> | 粉丝 <?php echo $vip['fans_count']; ?>
                                             | <?php if ($vip['online_state'] == true) {
                                                 echo '在线';
