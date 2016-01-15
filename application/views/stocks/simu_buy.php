ca66e700b387865cdb20e3606667aecda3118d5e
tangchaohong <444092487@qq.com>
2016-01-15 16:23:20 +0800

    update the index

--- application/views/stocks/simu_buy.php
+++ application/views/stocks/simu_buy.php
@@ -265,8 +265,7 @@
             else {
                 var response = data.st_info;
                 var bond_name = response.Symbol; //ajax获取证券名称
-                is_bond = data.is_bond; //判断是否为债券
-                alert(is_bond);
+                is_bond = (data.is_bond == 1) ? true: false; //判断是否为债券
                 var bond_cur_price = decimal(response.TradePrice); //获取最新价
                 var bond_lastday_price = decimal(response.PreClosePx); //获取昨日收盘价
                 var bond_highest = decimal(1.1 * bond_lastday_price); //涨停
