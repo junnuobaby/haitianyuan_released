cdc2c3f929abcc1b54d40bb89bf1bf993a775f11
tangchaohong <444092487@qq.com>
2016-01-25 17:27:36 +0800

    update

--- application/views/match/match_buy.php
+++ application/views/match/match_buy.php
@@ -289,8 +289,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
                     if(is_bond){
                         $('.bond_full_price').removeClass('hidden');
-                        alert(bond_cur_price + ',' + bond_interest);
-                        alert(bond_cur_price + bond_interest);
                         $('#bond_full_price').html(decimal_3(bond_cur_price + bond_interest));
                     }
 
