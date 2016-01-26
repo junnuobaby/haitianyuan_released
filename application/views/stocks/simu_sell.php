68de6272bca6d01a4206d1a421a3078108dc3e59
tangchaohong <444092487@qq.com>
2016-01-26 14:35:20 +0800

    update

--- application/views/stocks/simu_sell.php
+++ application/views/stocks/simu_sell.php
@@ -158,7 +158,8 @@ $sell_stocks = $sell_list;
             var selected_name = $(this).children('option:selected').data('name');
             var max_volume = $(this).children('option:selected').data('volume');
             var selected_cost = $(this).children('option:selected').data('cost');
-            is_bond = ($(this).children('option:selected').data('cost') == 1);
+            is_bond = ($(this).children('option:selected').data('identity') == 1);
+            alert(is_bond);
             //显示最大可卖出数量
             $('div.largest_quantity').removeClass('hidden');
             //显示买入该股票价格
